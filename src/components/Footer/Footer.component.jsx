8f78d6044993b103c710abc884c7e7822ed3d3ce
Parth Sarathi Dixit <110755500+parth-psd-009@users.noreply.github.com>
2025-09-29 19:06:48 +0530

    Update Footer.component.jsx

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -7,7 +7,7 @@ import Github from "../../../public/assets/github.svg";
 import Twitter from "../../../public/assets/twitter.svg";
 import Heart from "../../../public/assets/heart.svg";
 import LinkedinHover from "../../../public/assets/linkedin-fill.svg";
-import Wand from "../../../public/assets/wand.svg"; 
+import Wand from "../../../public/assets/wandwhite.svg"; 
 
 const Footer = () => {
     const [hover, setHover] = useState(false);
@@ -58,7 +58,7 @@ const Footer = () => {
                         className="social-img mx-4 md:mx-2"
                         target="_blank"
                     />
-                {/* </a>
+                </a>
                 <a 
                     href="https://open.spotify.com/playlist/0P5qC70RE5y6A7zPHDJUth?si=8058e1a182884649" 
                     target="_blank"
@@ -68,7 +68,7 @@ const Footer = () => {
                         alt="Spotify Playlist"
                         className="social-img mx-4 md:mx-2"
                     />
-                </a> */}
+                </a> 
             </div>
             {/* <div> */}
             <p className="font-extralight mt-12 md:mt-16 max-w-md mx-auto">

acc66429b6e8fad9a7dfbb6b8a892aea0dd41af2
Parth Sarathi Dixit <110755500+parth-psd-009@users.noreply.github.com>
2025-09-29 21:35:20 +0530

    Update MyCarousel.component.jsx

--- src/components/Carousel/MyCarousel.component.jsx
+++ src/components/Carousel/MyCarousel.component.jsx
@@ -10,7 +10,7 @@ import Web from "../../../public/assets/web.svg";
 const MyCarousel = () => {
     return (
         <Carousel
-            className="p-10 mb-20"
+            className="p-6 mt-10 mb-16"
             infiniteLoop={true}
             showIndicators={false}
             showStatus={false}

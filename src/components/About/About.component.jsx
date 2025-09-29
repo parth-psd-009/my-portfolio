3056051c844ceb6123dd0035108bcc56dad2273e
Parth Sarathi Dixit <110755500+parth-psd-009@users.noreply.github.com>
2025-09-29 21:52:52 +0530

    Update About.component.jsx

--- src/components/About/About.component.jsx
+++ src/components/About/About.component.jsx
@@ -8,7 +8,7 @@ const About = () => {
             <h1 className="section-heading text-2xl text-center mb-16 md:mb-24 md:text-6xl">
                 About Me
             </h1>
-            <p className="section-text px-6 text-base text-center my-10 md:px-36 md:text-xl md:my-10">
+            <p className="section-text px-6 text-base text-center md:px-36 md:text-xl md:my-10">
                 Hi, I’m Parth Sarathi Dixit, a final-year student at the LNM Institute of Information Technology. 
                 I have a strong interest in technology and love learning by exploring different fields. My main focus areas are web development, 
                 machine learning, and deep learning, but I also enjoy working on UI/UX design, graphic design, and 3D modeling.

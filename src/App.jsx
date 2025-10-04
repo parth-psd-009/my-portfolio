dcc314f1c7594c39e93f262812f283ba231e5bb5
Parth Sarathi Dixit <110755500+parth-psd-009@users.noreply.github.com>
2025-10-04 18:43:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,33 @@ import About from "./components/About/About.component";
 import Stacks from "./components/Stacks/Stacks.component";
 import Experiences from "./components/Experiences/Experiences.component";
 import Footer from "./components/Footer/Footer.component";
+import BlobBackground from "./components/BlobBackground/BlobBackground.component"; 
 
 function App() {
     return (
         <Router>
             <div className="main-div bg-black text-white">
+                {/* Blob background */}
+                <BlobBackground />
+
                 <Navbar />
+
                 <div id="hero">
                     <Hero />
                 </div>
+
                 <div id="about">
                     <About />
                 </div>
+
                 {/* <div id="stacks">
                     <Stacks />
                 </div> */}
+
                 <div id="experiences">
                     <Experiences />
                 </div>
+
                 <div id="footer">
                     <Footer />
                 </div>
